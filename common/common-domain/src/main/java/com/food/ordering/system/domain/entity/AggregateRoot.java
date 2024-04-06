a772f0fc8a21f54867218cbe0d42038cb934443b
dunky-star <dunkygeoffrey39@gmail.com>
2024-04-06 15:34:49 -0500

    Aggregare Root extending BaseEntity class

--- common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -1,5 +1,7 @@
 package com.food.ordering.system.domain.entity;
 
 // To distinguish root objects from other entities.
-public abstract class AggregateRoot<ID>  extends BaseEntity<ID>{
+
+public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
+
 }
