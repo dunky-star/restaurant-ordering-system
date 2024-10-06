94838ad64fc3cd3176ca126345ff6c5b03915ff1
dunky-star <dunkygeoffrey39@gmail.com>
2024-10-05 22:19:31 -0500

    Added Order Entity implementation methods

--- restaurant-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
+++ restaurant-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/Money.java
@@ -9,6 +9,8 @@ import java.util.Objects;
 public class Money {
     private final BigDecimal amount;
 
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
+
     public Money(BigDecimal amount) {
         this.amount = amount;
     }
--- restaurant-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/service/domain/entity/Order.java
+++ restaurant-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/service/domain/entity/Order.java
@@ -2,10 +2,13 @@ package com.food.ordering.system.service.domain.entity;
 
 import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valueobject.*;
+import com.food.ordering.system.service.domain.exception.OrderDomainException;
+import com.food.ordering.system.service.domain.valueobject.OrderItemId;
 import com.food.ordering.system.service.domain.valueobject.StreetAddress;
 import com.food.ordering.system.service.domain.valueobject.TrackingId;
 
 import java.util.List;
+import java.util.UUID;
 
 // AggregateRoot for Order Aggregates
 public class Order extends AggregateRoot<OrderId> {
@@ -17,6 +20,53 @@ public class Order extends AggregateRoot<OrderId> {
     private TrackingId trackingId;
     private OrderStatus orderStatus;
     private List<String> failureMessages;
+
+    public void initializeOrder() {
+        setId(new OrderId(UUID.randomUUID()));
+        trackingId = new TrackingId(UUID.randomUUID());
+        orderStatus = OrderStatus.PENDING;
+        initializeOrderItems();
+    }
+
+    public void validateOrder() {
+        validateInitialOrder();
+        validateTotalPrice();
+        validateItemsPrice();
+    }
+
+    private void validateInitialOrder() {
+        if (orderStatus != null || getId() != null) {
+            throw new OrderDomainException("Order is not in correct state for initialization!");
+        }
+    }
+
+    private void validateTotalPrice() {
+        if (price == null || !price.isGreaterThanZero()) {
+            throw new OrderDomainException("Total price must be greater than zero!");
+        }
+    }
+
+    private void validateItemsPrice() {
+        items.stream().map(orderItem -> {
+            validateItemPrice(orderItem);
+        });
+    }
+    
+    private void validateItemPrice(OrderItem orderItem) {
+        if (!orderItem.isPriceValid()) {
+            throw new OrderDomainException("Order item price: " + orderItem.getPrice().getAmount() +
+                    " is not valid for product " + orderItem.getProduct().getId().getValue());
+        }
+    }
+
+
+    private void initializeOrderItems() {
+        long itemId = 1;
+        for (OrderItem orderItem: items) {
+            orderItem.initializeOrderItem(super.getId(), new OrderItemId(itemId++));
+        }
+    }
+
 
     private Order(Builder builder) {
         super.setId(builder.orderId);
