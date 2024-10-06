2c52856b6eaf26e16f3ccae876daf05bf48a5189
dunky-star <dunkygeoffrey39@gmail.com>
2024-10-05 22:27:11 -0500

    Added Order Entity implementation methods

--- restaurant-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/service/domain/entity/Order.java
+++ restaurant-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/service/domain/entity/Order.java
@@ -47,11 +47,18 @@ public class Order extends AggregateRoot<OrderId> {
     }
 
     private void validateItemsPrice() {
-        items.stream().map(orderItem -> {
+        Money orderItemsTotal = items.stream().map(orderItem -> {
             validateItemPrice(orderItem);
-        });
+            return orderItem.getSubTotal();
+        }).reduce(Money.ZERO, Money::add);
+
+        if (!price.equals(orderItemsTotal)) {
+            throw new OrderDomainException("Total price: " + price.getAmount()
+                    + " is not equal to Order items total: " + orderItemsTotal.getAmount() + "");
+        }
     }
-    
+
+
     private void validateItemPrice(OrderItem orderItem) {
         if (!orderItem.isPriceValid()) {
             throw new OrderDomainException("Order item price: " + orderItem.getPrice().getAmount() +
