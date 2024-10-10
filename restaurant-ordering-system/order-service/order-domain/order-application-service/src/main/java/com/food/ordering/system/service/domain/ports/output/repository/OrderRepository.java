7a2796fa3d4d766fa6ec676b170c4602eeaad1b6
dunky-star <dunkygeoffrey39@gmail.com>
2024-10-09 22:37:58 -0500

    Added ports and Adapters

---
+++ restaurant-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/service/domain/ports/output/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.food.ordering.system.service.domain.ports.output.repository;
+
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.service.domain.entity.Order;
+import com.food.ordering.system.service.domain.valueobject.TrackingId;
+
+import java.util.Optional;
+
+public interface OrderRepository {
+    Order save(Order order);
+
+    Optional<Order> findById(OrderId orderId);
+
+    Optional<Order> findByTrackingId(TrackingId trackingId);
+}
