6d61313eec8502e5d539c704b984a8cb1b834217
dunky-star <dunkygeoffrey39@gmail.com>
2024-10-22 05:25:53 -0700

    Completed DTO and Message Listeners input and output adapters

---
+++ restaurant-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/service/domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.service.domain.ports.output.message.publisher.payment;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+
+public interface OrderCreatedPaymentRequestMessagePublisher extends DomainEventPublisher {
+
+}
