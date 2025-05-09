2ffbd835cc4022d2de3a2f9c379931c8eb02dc71
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-05-09 00:31:16 -0500

    feat(Order-Service): Create Entities and ValueObjects

--- restaurant-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
+++ restaurant-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/entity/AggregateRoot.java
@@ -1,5 +1,6 @@
 package com.food.ordering.system.domain.entity;
 
+// Marker class
 public abstract class AggregateRoot<ID> extends BaseEntity<ID> {
     // AggregateRoot is a type of entity that is the root of an aggregate in Domain-Driven Design (DDD).
     // It is responsible for maintaining the consistency of the aggregate and enforcing business rules.
