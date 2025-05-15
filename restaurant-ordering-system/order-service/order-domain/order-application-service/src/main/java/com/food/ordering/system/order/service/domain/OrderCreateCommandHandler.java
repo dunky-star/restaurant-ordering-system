6beef0f7aeb5570a024d93340bdb929c78e9ff76
Geoffrey Duncan Opiyo <dunkygeoffrey39@gmail.com>
2025-05-14 22:28:25 -0500

    feat(Order-Service): Implement Order Applcation Service input ports

--- restaurant-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ restaurant-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -14,6 +14,7 @@ import com.food.ordering.system.order.service.domain.ports.output.repository.Res
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -39,6 +40,7 @@ public class OrderCreateCommandHandler {
         this.restaurantRepository = restaurantRepository;
     }
 
+    @Transactional
     public CreateOrderResponse createOrder(CreateOrderCommand createOrderCommand) {
         checkCustomer(createOrderCommand.getCustomerId());
         Restaurant restaurant = checkRestaurant(createOrderCommand);
